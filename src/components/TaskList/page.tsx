cabed41ea1b7e882ea233035e302598ca5768979
Ronald <ronaldzamoradev@gmail.com>
2023-02-22 12:19:19 -0500

    fix

--- src/components/TaskList/page.tsx
+++ src/components/TaskList/page.tsx
@@ -1,11 +1,13 @@
 import { CardTask } from '@/components'
 import style from './style.module.scss'
 
-export default function TaskList ({ todos, setTodos }: any) {
-  const list = todos?.map((todo: any, index: number) => <CardTask key={todo.id ? todo.id : index} todo={todo} todos={todos} setTodos={setTodos} />)
+export default function TaskList ({ todos, setTodos, userId }: any) {
+  const filter = todos?.filter((todo: any) => todo.user_id === userId)
+  const list = filter?.map((todo: any, index: number) => <CardTask key={todo.id ? todo.id : index} todo={todo} todos={todos} setTodos={setTodos} />)
+  console.log('list: ', list)
   return (
     <div className={style.container}>
-      {list.length === 0 ? <h3>You don't have to do</h3> : list}
+      {list?.length === 0 ? <h3>You don't have to do</h3> : list}
     </div>
   )
 }
