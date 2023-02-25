90aa25b860f4509485cba508f3f3a747c9230b83
Ronald <ronaldzamoradev@gmail.com>
2023-02-25 18:49:41 -0500

    fix: list organization

--- src/components/TaskList/page.tsx
+++ src/components/TaskList/page.tsx
@@ -2,7 +2,7 @@ import { CardTask } from '@/components'
 import style from './style.module.scss'
 
 export default function TaskList ({ todos, userId }: any) {
-  const Mytodos = todos?.filter((todo: any) => todo.user_id === userId).sort((a: any, b: any) => a - b)
+  const Mytodos = todos?.filter((todo: any) => todo.user_id === userId).sort((a: any, b: any) => a.id - b.id)
   const list = Mytodos?.map((todo: any, index: number) => <CardTask key={todo.id ? todo.id : index} todo={todo} />)
 
   return (
